f1352ff0d374a2a411587134f3f5312243a8b2c8
Matt Jarvis <mattj23@gmail.com>
2022-12-05 18:35:25 -0500

    opens ssh terminal

--- quick_manage/cli/ssh.py
+++ quick_manage/cli/ssh.py
@@ -1,3 +1,8 @@
+import os
+from contextlib import contextmanager
+from typing import List
+import subprocess
+
 import click
 from click import Context
 
@@ -5,19 +10,46 @@ from quick_manage.environment import Environment, echo_line
 from quick_manage.ssh.client import SSHClient
 from quick_manage.cli.common import HostNameType, StoreVarType, KeyNameType
 
+from tempfile import NamedTemporaryFile
+
+
+@contextmanager
+def temp_key_file():
+    temp_file = NamedTemporaryFile(mode="w", delete=False)
+    try:
+        yield temp_file
+    finally:
+        temp_file.close()
+        os.remove(temp_file.name)
+
 
 @click.command(name="ssh")
 @click.argument("host", type=HostNameType())
-@click.argument("command", type=str)
+@click.argument("commands", nargs=-1)
 @click.pass_context
-def main(ctx: Context, host: str, command: str):
+def main(ctx: Context, host: str, commands: List[str]):
     env = Environment.default()
     host = env.get_host(host)
-    client: SSHClient = host.get_client("ssh")
-    conn = client.connect()
-    conn.run(command)
 
+    if commands:
+        client: SSHClient = host.get_client("ssh")
+        conn = client.connect()
+        for c in commands:
+            conn.run(c)
+    else:
+        client_configs = [x for x in host.config.client if x['type'] == "ssh"]
+        if not client_configs:
+            echo_line(env.fail(f"No ssh configurations in host {host.config.host}"))
+            return
 
+        cfg = client_configs[0]
+        key = env.get_key(cfg["key"])
+        with temp_key_file() as key_file:
+            key_file.write(key)
+            key_file.close()
 
-
-
+            os.chmod(key_file.name, 0o600)
+            echo_line(env.visible(f"Opening SSH terminal to {host.config.host}"))
+            ssh_cmd = f'ssh -i {key_file.name} -o BatchMode=yes -p 22 {cfg["user"]}@{host.config.host} 2> ssh-error.log'
+            echo_line(env.visible(f" > {ssh_cmd}"), "\n")
+            subprocess.run(ssh_cmd, shell=True)
