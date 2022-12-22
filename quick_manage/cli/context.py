59c891434d06dc21b4a890610209792a3b0ced14
Matt Jarvis <mattj23@gmail.com>
2022-12-22 02:20:38 -0500

    more context

--- quick_manage/cli/context.py
+++ quick_manage/cli/context.py
@@ -1,14 +1,25 @@
 """
     The context related commands
 """
+from typing import List
 
 import click
+from click import ParamType, Parameter
+from click.shell_completion import CompletionItem
+
 from ..environment import Environment, echo_line, echo_json
 
 
-@click.group(name="context", invoke_without_command=True)
+@click.group(name="context")
 @click.pass_context
 def main(ctx: click.Context):
+    pass
+
+
+@main.command(name="list")
+@click.pass_context
+def list_contexts(ctx: click.Context):
+    """ List all available contexts in the configuration """
     env = Environment.default()
 
     echo_line(env.head("Contexts:"))
@@ -21,4 +32,18 @@ def main(ctx: click.Context):
     echo_line()
 
 
+class ContextNameType(ParamType):
+    name = "context-name"
 
+    def shell_complete(self, ctx: click.Context, param: Parameter, incomplete: str) -> List[CompletionItem]:
+        env = Environment.default()
+        return [CompletionItem(x.name) for x in env.config.contexts if x.name.startswith(incomplete)]
+
+
+@main.command(name="set")
+@click.pass_context
+@click.argument("name", type=ContextNameType())
+def set_context(ctx: click.Context, name: str):
+    """ Set one of the available contexts to be the active focus """
+    env = Environment.default()
+    echo_line(f"Setting context to {name}")
