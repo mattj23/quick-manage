ecaf3ded0b12f08ee634a78fc0f8e1dd353d0e7e
Matt Jarvis <mattj23@gmail.com>
2022-12-21 22:44:38 -0500

    reworking config

---
+++ quick_manage/serialization.py
@@ -0,0 +1,30 @@
+"""
+    Serialization tools
+"""
+from io import StringIO
+from typing import Dict, Union
+
+import ruamel.yaml as yaml
+from dataclasses import asdict as _as_dict
+from dacite import from_dict
+from typing.io import TextIO
+
+
+def as_dict_strip(obj) -> Dict:
+    return _as_dict(obj, dict_factory=lambda x: {k: v for (k, v) in x if v is not None})
+
+
+def to_yaml(obj, target: Union[TextIO, StringIO, str]):
+    if isinstance(target, str):
+        with open(target, "w") as handle:
+            yaml.dump(as_dict_strip(obj), handle)
+    else:
+        yaml.dump(as_dict_strip(obj), target)
+
+
+def from_yaml(data_cls, target: Union[TextIO, StringIO,  str]):
+    if isinstance(target, str):
+        with open(target, "r") as handle:
+            return from_dict(data_cls, yaml.load(handle))
+    else:
+        return from_dict(data_cls, yaml.load(target))
